0e7f7c607358d749f66422da74cd8c912db2130c
Rupinder Singh <rupindersingh0853@gmail.com>
2023-11-23 16:35:06 +0530

    Added routes to the main server file

--- server.js
+++ server.js
@@ -31,6 +31,8 @@ app.get("/", (req, res) => {
   res.json({ message: "Welcome to nodeJS application." });
 });
 
+require("./app/routes/turorial.routes")(app);
+
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
